ee59ebfe96c51bf377353a8c8a1dfba8e69f92c0
ag3nt3154@gmail.com <ag3nt3154@gmail.com>
2020-07-24 22:09:06 +0800

    added working operators

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 console.log("hello world");
 
+let curVal = 0;
+let curOp = null;
+let newVal = 0;
+
 const num_keys = document.querySelectorAll(".num");
 const disp = document.querySelector("#display");
 
 num_keys.forEach((key) => {
     key.addEventListener("click", function (e) {
         display(e.target.textContent);
+        newVal = updateNewVal(newVal, e.target.textContent)
     });
 });
 
@@ -17,12 +22,28 @@ clearBtn.addEventListener("click", function (e) {
 const opKeys = document.querySelectorAll(".op");
 opKeys.forEach((key) => {
     key.addEventListener("click", function (e) {
-        display(e.target.textContent);
+        // display(e.target.textContent);
+        clear();
+        curVal = operate(curVal, curOp, newVal);
+        newVal = 0;
+        disp.textContent = curVal;
+        curOp = e.target.textContent;
     });
 });
 
+const enterBtn = document.querySelector("#enter");
+enterBtn.addEventListener("click", function (e) {
+    clear();
+    curVal = operate(curVal, curOp, newVal);
+    newVal = 0;
+    disp.textContent = curVal;
+    curOp = null;
+});
 
 
+function updateNewVal(curVal, digit) {
+    return parseFloat(curVal + digit)
+}
 
 function display(newValue) {
     let prev_string = disp.textContent;
@@ -34,3 +55,29 @@ function clear() {
     disp.textContent = null;
 }
 
+function operate(curVal, opn, newVal) {
+    console.log(curVal, opn, newVal);
+    // curVal = parseFloat(curVal);
+    // newVal = parseFloat(newVal);
+
+    if (opn == null) {
+        curVal = newVal;
+    }
+    else if (opn == "+") {
+        curVal = curVal + newVal;
+    }
+    else if (opn == "-") {
+        curVal = curVal - newVal;
+    }
+    else if (opn == "*") {
+        curVal = curVal * newVal;
+    }
+    else if (opn == "/") {
+        curVal = curVal / newVal;
+    }
+
+    // curVal = toString(curVal);
+
+    console.log(curVal);
+    return curVal;
+}
